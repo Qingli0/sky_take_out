fab4ec7869e10e0783ee7e787f9428fb575cf665
qingli <1175641066@qq.com>
2025-03-20 17:02:08 +0800

    新增员工业务代码开发

--- sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
@@ -1,22 +1,31 @@
 package com.sky.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@ApiModel(description = "添加新用户模块")
 public class EmployeeDTO implements Serializable {
 
+    @ApiModelProperty("用户id")
     private Long id;
 
+    @ApiModelProperty("用户名称")
     private String username;
 
+    @ApiModelProperty("用户身份")
     private String name;
 
+    @ApiModelProperty("用户手机号码")
     private String phone;
 
+    @ApiModelProperty("用户性别")
     private String sex;
 
+    @ApiModelProperty("用户身份证")
     private String idNumber;
 
 }
